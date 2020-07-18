de8c3bebb7700dfb659377fcce56815d290d27da
Marcos <mtomasferreira@hotmail.com>
2020-07-18 14:14:38 -0300

    novo projeto e splash

---
+++ WSTowerApp/WSTowerApp/WSTowerApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using WSTowerApp.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WSTowerApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Splash();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
